3c866485250aef7fd682bcde262039881528b975
MohsenArvan <F.n.rayat@gmail.com>
2023-10-16 13:40:13 +0330

    migrate table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('username')->unique();
+            $table->string('title');
+            $table->string('about'); 
+            $table->text('subtitle'); 
+            $table->text('image'); 
             $table->rememberToken();
             $table->timestamps();
         });
